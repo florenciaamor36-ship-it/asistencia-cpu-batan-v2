b75b35f12e3f1cf3da66a43a0ee5625621c5d445
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-08 21:33:17 -0300

    Make PWA installable instead of shortcut

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,14 @@
-const CACHE = 'cpu-batan-v1';
-self.addEventListener('install', event => self.skipWaiting());
+const CACHE = 'cpu-batan-v2';
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './manifest.webmanifest'])).catch(() => {}));
+  self.skipWaiting();
+});
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
